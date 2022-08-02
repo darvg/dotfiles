fed2a9de52d86299f0df6ff938315de9ef28c0ff
Siddharth Gollapudi <siddharthgollapudi99@gmail.com>
2022-08-02 16:15:37 +0530

    Change vim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,280 @@
-require("mappings")
-require("options")
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
-require("plugins")
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+vim.cmd [[
+  augroup Packer
+    autocmd!
+    autocmd BufWritePost init.lua PackerCompile
+  augroup end
+]]
+
+
+
+-- PACKAGES
+local use = require('packer').use
+require('packer').startup(function()
+  -- package manager
+  use 'wbthomason/packer.nvim'
+
+  -- colorscheme
+  use 'folke/tokyonight.nvim'
+
+  -- statusline
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  -- region commenting
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+        require('Comment').setup()
+    end
+  }
+
+	-- telescope/picking
+	use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+  -- treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    requires = { 'nvim-treesitter/nvim-treesitter-textobjects' }
+  }
+
+  -- LSP
+	use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+
+	-- fancy error lines
+	use {
+  	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  	config = function()
+    	require("lsp_lines").setup()
+  	end,
+	}
+	
+	-- autopairs
+  use {
+		'windwp/nvim-autopairs',
+	}
+  
+end)
+
+
+
+-- COLORSCHEME
+vim.g.tokyonight_style = "night"
+vim.cmd[[colorscheme tokyonight]]
+
+
+
+-- STATUSLINE
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'tokyonight',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+
+
+-- AUTOPAIRS
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+require('nvim-autopairs').setup{}
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
+
+
+-- TREESITTER
+require'nvim-treesitter.configs'.setup {
+  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {},
+
+  -- Install languages synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- List of parsers to ignore installing
+  ignore_install = { "javascript" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- list of language that will be disabled
+    disable = { "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+  callback = function()
+    vim.opt.foldmethod     = 'expr'
+    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+  end
+})
+
+
+
+-- LSP/COMPLETION
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.setup()
+
+
+-- TELESCOPE
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    },
+
+		vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case"
+    },
+		file_ignore_patterns = {"node_modules", ".git"},
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
+require('telescope').load_extension 'fzf'
+
+
+
+
+
+-- OPTIONS
+local cmd = vim.cmd
+local indent = 2
+
+local opt = vim.opt
+local g = vim.g
+
+-- global settings
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+-- local settings
+opt.guifont = "Iosevka Custom:h12"
+opt.grepprg = "rg --vimgrep"
+opt.grepformat = "%f:%l:%c:%m"
+opt.hidden = true
+opt.confirm = true
+opt.cursorline = true
+opt.swapfile = true
+opt.mouse = "a"
+opt.number = true
+opt.foldmethod = "manual"
+opt.ruler = false
+opt.ignorecase = true
+opt.pumblend = 10
+opt.pumheight = 8
+-- opt.signcolumn = "yes"
+opt.showmode = false
+opt.cmdheight = 1
+
+opt.smartcase = true
+opt.shiftround = true
+opt.shiftwidth = indent
+opt.tabstop = indent
+opt.smartindent = true
+
+opt.termguicolors = true
+
+
+
+-- MAPPINGS
+-- generics
+vim.api.nvim_set_keymap('n', '<leader>v', [[<cmd>vsplit<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>h', [[<cmd>split<CR>]], { noremap = true, silent = true })
+
+-- telescope
+vim.api.nvim_set_keymap('n', '<leader>pf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ps', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>pt', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
